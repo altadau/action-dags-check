88c685adb5ad3e22d002e596460ab62916ad4463
Altayev <Dauren.Altayev@bakerhughes.com>
2023-11-22 13:40:38 -0500

    Check line emptiness

--- src/check-airflow-dags-tags.py
+++ src/check-airflow-dags-tags.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+ 
 def find_dag_files(directory):
     dag_files = []
     for root, dirs, files in os.walk(directory):
@@ -8,12 +9,14 @@ def find_dag_files(directory):
             if file.endswith(".py"):
                 dag_files.append(os.path.join(root, file))
     return dag_files
+ 
 def find_emr_tags_in_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
         tags_pattern = re.compile(r'["\'](?:Tags|tags)["\']\s*:\s*\[([^]]*)\]', re.MULTILINE)
         matches = tags_pattern.findall(content)
         return matches
+ 
 def find_tags_in_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
@@ -23,18 +26,22 @@ def find_tags_in_file(file_path):
             tags_pattern = re.compile(r'tags\s*=\s*\(([^\)]*)\)', re.IGNORECASE)
             matches = tags_pattern.findall(content)
         return matches
+ 
 def find_sec_conf_in_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
+ 
         security_config_pattern = re.compile(r'"SecurityConfiguration"\s*:\s*([^\n,]*)')
         match = security_config_pattern.search(content)
+ 
         if match:
             security_config_value = match.group(1).strip().strip('\'"')
             print(f"SecurityConfiguration: {security_config_value}")
-            return True
+            return security_config_value  # Возвращаем значение, чтобы проверить, пусто ли поле
         else:
             print("SecurityConfiguration not found in the file.")
-            return False
+            return None
+ 
 dag_directory = "orchestration/dags"
 dag_files = find_dag_files(dag_directory)
  
@@ -47,14 +54,16 @@ for dag_file in dag_files:
     emr_tags = find_emr_tags_in_file(dag_file)    
     formatted_emr_tags = json.dumps(emr_tags, indent=2, ensure_ascii=False)
     print(f"EMR Tags in {dag_file}:\n{formatted_emr_tags}")
+ 
     tags = find_tags_in_file(dag_file)
     print(f"Tags in {dag_file}: {tags}")
+ 
     found_sec_conf = find_sec_conf_in_file(dag_file)
  
     # Обновляем флаги
-    emr_tags_found = emr_tags_found or bool(emr_tags)
-    tags_found = tags_found or bool(tags)
-    sec_conf_found = sec_conf_found or found_sec_conf
+    emr_tags_found = emr_tags_found or bool(emr_tags and any(emr_tags))
+    tags_found = tags_found or bool(tags and any(tags))
+    sec_conf_found = sec_conf_found or bool(found_sec_conf)
  
 # Проверяем флаги и завершаем выполнение с ошибкой, если не найдены соответствующие элементы
 if not dag_files:
@@ -62,13 +71,13 @@ if not dag_files:
     exit(1)
  
 if not emr_tags_found:    
-    print("::error::No EMR Tags found in any of the Airflow DAG files.")
+    print("::error::No non-empty EMR Tags found in any of the Airflow DAG files.")
     exit(1)
  
 if not tags_found:    
-    print("::error::No Airflow Tags found in any of the Airflow DAG files.")    
+    print("::error::No non-empty Airflow Tags found in any of the Airflow DAG files.")    
     exit(1)
  
 if not sec_conf_found:    
-    print("::error::SecurityConfiguration not found in any of the Airflow DAG files.")
+    print("::error::No non-empty SecurityConfiguration found in any of the Airflow DAG files.")
     exit(1)
