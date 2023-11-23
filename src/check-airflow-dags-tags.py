d2be1761d6db8e0f3190ba4044f5d2fe9e295c06
Altayev <Dauren.Altayev@bakerhughes.com>
2023-11-23 11:10:01 -0500

    Compare EMR Tags in DAG 2

--- src/check-airflow-dags-tags.py
+++ src/check-airflow-dags-tags.py
@@ -58,6 +58,7 @@ if not dag_files:
 for dag_file in dag_files:
     emr_tags_str_in_file = find_emr_tags_in_file(dag_file)
     combined_tags_str = ''.join(emr_tags_str_in_file)
+    combined_tags_str = combined_tags_str.replace("'", "\"")
     print(f"EMR Tags in {dag_file} (raw): {combined_tags_str}")
     emr_tags_in_file = json.loads(combined_tags_str) if combined_tags_str else []
     formatted_emr_tags = json.dumps(emr_tags_in_file, indent=2, ensure_ascii=False)
