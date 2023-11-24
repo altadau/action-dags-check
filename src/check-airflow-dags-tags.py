6d51974b34b66f5e7bb75437cd1f94c9d2c9e660
Altayev <Dauren.Altayev@bakerhughes.com>
2023-11-23 19:47:15 -0500

    Search EMR Tags in DAG 21

--- src/check-airflow-dags-tags.py
+++ src/check-airflow-dags-tags.py
@@ -1,7 +1,6 @@
 import os
 import re
 import json
-import sys
  
 def find_dag_files(directory):
     dag_files = []
@@ -14,61 +13,57 @@ def find_dag_files(directory):
 def find_emr_tags_in_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
-        print(f"Content of {file_path}: {content}")
         tags_pattern = re.compile(r'["\'](?:Tags|tags)["\']\s*:\s*\[([^]]*)\]', re.MULTILINE)
         matches = tags_pattern.findall(content)
-        return json.loads(matches[0]) if matches else []
+        return matches
  
-def find_security_configuration(file_path):
+def find_tags_in_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
-        security_pattern = re.compile(r'["\']SecurityConfiguration["\']\s*:\s*["\'](.*?)["\']', re.MULTILINE)
-        matches = security_pattern.findall(content)
-        return matches[0] if matches else None
+        tags_pattern = re.compile(r'tags\s*=\s*\[([^\]]*)\]', re.IGNORECASE)
+        matches = tags_pattern.findall(content)
+        if not matches:
+            tags_pattern = re.compile(r'tags\s*=\s*\(([^\)]*)\)', re.IGNORECASE)
+            matches = tags_pattern.findall(content)
+        return matches
  
-directory = os.environ.get('DIRECTORY', '')
-dag_files = find_dag_files(directory)
-empty_value_found = False
-emr_tags_not_found = []
+def find_sec_conf_in_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
  
-security_configurations = set()
-airflow_emr_tags = set()
+        security_config_pattern = re.compile(r'"SecurityConfiguration"\s*:\s*([^\n,]*)')
+        match = security_config_pattern.search(content)
  
-if not dag_files:
-    print(f"::error::No DAG files found in the specified directory: {directory}")
+        if match:
+            security_config_value = match.group(1).strip().strip('\'"')
+            print(f"SecurityConfiguration: {security_config_value}")
+            return security_config_value
+        else:
+            print("SecurityConfiguration not found in the file.")
+            return None
+ 
+dag_directory = os.environ.get('DIRECTORY', '')
+dag_files = find_dag_files(dag_directory)
+ 
+
+found_values = []
  
 for dag_file in dag_files:
-    emr_tags_in_file = find_emr_tags_in_file(dag_file)
-    security_configuration = find_security_configuration(dag_file)
+    emr_tags = find_emr_tags_in_file(dag_file)    
+    formatted_emr_tags = json.dumps(emr_tags, indent=2, ensure_ascii=False)
+    print(f"EMR Tags in {dag_file}:\n{formatted_emr_tags}")
  
-    if security_configuration:
-        security_configurations.add(security_configuration)
+    tags = find_tags_in_file(dag_file)
+    print(f"Tags in {dag_file}: {tags}")
  
-    print(f"EMR Tags in {dag_file} (raw): {emr_tags_in_file}")
-    print(f"EMR Tags in {dag_file}: {json.dumps(emr_tags_in_file, indent=2)}")
-    print(f"Security Configuration in {dag_file}: {security_configuration}")
+    found_sec_conf = find_sec_conf_in_file(dag_file)
  
-    if emr_tags_in_file:
-        with open('airflowemrtags.txt', 'w') as airflow_emr_tags_file:
-            json.dump(emr_tags_in_file, airflow_emr_tags_file, indent=2)
+    found_values.extend(filter(None, [emr_tags, tags, found_sec_conf]))
  
-        airflow_emr_tags.update(set(emr_tags_in_file))
+if not dag_files:
+    print("::error::No DAG files found in the specified directory.")
+    exit(1)
  
-if empty_value_found or emr_tags_not_found:
-    if empty_value_found:
-        print("::error::At least one empty value found in Airflow DAG files.")
- 
-    if emr_tags_not_found:
-        print(f"::error::EMR Tags not found in Airflow DAG files: {emr_tags_not_found}")
- 
-    sys.exit(1)
- 
-input_emr_tags = [
-    {"Key": "ssmmanaged", "Value": "no see CSRC_DBC_933_EC2_SSM_MANAGED"},
-    {"Key": "CSRC_DBC_933", "Value": "CSRC_DBC_933_EC2_SSM_MANAGED"}
-]
- 
-missing_tags = [tag for tag in input_emr_tags if tag not in airflow_emr_tags]
- 
-print(f"Security Configurations found: {security_configurations}")
-print(f"Missing EMR Tags in Airflow DAG: {missing_tags}")
+if not any(found_values):
+    print("::error::No non-empty values found in any of the Airflow DAG files.")
+    exit(1)
